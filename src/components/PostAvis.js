c350bce6f07fbde506034c1a195f71e053a121c4
Kiwiii-dot <75044328+Kiwiii-dot@users.noreply.github.com>
2021-12-31 17:57:20 -0500

    anim

--- src/components/PostAvis.js
+++ src/components/PostAvis.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
+import {gsap} from "gsap"
 const PostAvis = () => {
+    let form = useRef()
+
+    useEffect(() => {
+        gsap.from(form, {
+            width:0,
+            duration: 1.5
+        })
+    }, []);
+
     return (
-        <div className='post-avis'>
+        <div className='post-avis' ref={el => {form = el}}>
             <form className='avis-form'>
                 <input type="text" className='push-avis' placeholder='exemple: Bon service, je vous le recommande'/>
                 <input type="text" className='push-pseudo' placeholder='exemple: Jean-luc'/>
