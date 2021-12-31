eb65300b5ee2c27634bf59b010d4a47a99d60b1b
Kiwiii-dot <75044328+Kiwiii-dot@users.noreply.github.com>
2021-12-31 18:35:28 -0500

    form finish

--- src/components/PostAvis.js
+++ src/components/PostAvis.js
@@ -3,6 +3,27 @@ import React, { useEffect, useRef } from 'react';
 import {gsap} from "gsap"
 const PostAvis = () => {
     let form = useRef()
+    let state = {
+        avis : '',
+        pseudo: '',
+        id: '',
+    }
+
+    const handleAvisChange = (e) =>{
+        state.avis = e.target.value
+    }
+
+    const handlePseudoChange = (e) =>{
+        state.pseudo = e.target.value
+    }
+
+    const handleIdChange = (e) =>{
+        state.id = e.target.value
+    }
+
+    const handleSubmit = (e) => {
+        alert(`${state.avis} ${state.pseudo} ${state.id}`)
+    }
 
     useEffect(() => {
         gsap.from(form, {
@@ -13,10 +34,10 @@ const PostAvis = () => {
 
     return (
         <div className='post-avis' ref={el => {form = el}}>
-            <form className='avis-form' action='/avis/post' method='post'>
-                <input type="text" name='avis' className='push-avis' placeholder='exemple: Bon service, je vous le recommande'/>
-                <input type="text" name='pseudo' className='push-pseudo' placeholder='exemple: Jean-luc'/>
-                <input type="text" name='id' className='push-id' placeholder='exemple: 12DS9398'/>
+            <form className='avis-form' onSubmit={handleSubmit}>
+                <input type="text" onChange={handleAvisChange} name='avis' className='push-avis' placeholder='exemple: Bon service, je vous le recommande'/>
+                <input type="text" onChange={handlePseudoChange} name='pseudo' className='push-pseudo' placeholder='exemple: Jean-luc'/>
+                <input type="text" onChange={handleIdChange} name='id' className='push-id' placeholder='exemple: 12DS9398'/>
                 <input type="submit" className='sub'/>
             </form>
         </div>
